ab4496655a827dd3d0f36e6d4f14bc6ab6e3c635
ronaldo <ronaldo.panuelos@yahoo.com>
2026-06-21 21:12:37 +0800

    Layout edited.

--- resources/views/hereafterlogin.blade.php
+++ resources/views/hereafterlogin.blade.php
@@ -1,17 +1,19 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard - here') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app1')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Here after login will show") }}
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+@section('content')
+<div class="row">
+	<div class="col-lg-12">
+		<div class="card mb-4">
+			<div class="card-header border-0">
+				<div class="d-flex justify-content-between">
+					<h3 class="card-title">This is the landing page after login success.</h3>
+					<a href="javascript:void(0);" class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">View Report</a>
+				</div>
+			</div>
+			<div class="card-body">
+
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
